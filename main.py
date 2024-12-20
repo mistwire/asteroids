be18d498fe6574d0cb69360921b7c445268dcea9
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-12-20 18:23:05 -0500

    shot & asteroid collisions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 import player
 from asteroid import Asteroid
 import asteroidfield
+import shot 
 
 def main():
     pygame.init()
@@ -14,10 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     player.Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     asteroidfield.AsteroidField.containers = updatable
+    shot.Shot.containers = (updatable, drawable)
 
     asteroid_field = asteroidfield.AsteroidField()
     player_1 = player.Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
@@ -33,7 +36,10 @@ def main():
             if i.collides_with(player_1):
                 print("Game over!")
                 sys.exit()
-
+            for s in shots:
+                if i.collides_with(s):
+                    i.kill()
+                    s.kill()
 
         # create a black screen:
         screen.fill((0,0,0))
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame 
 import circleshape
 from constants import *
+from shot import Shot
 
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -37,5 +39,18 @@ class Player(circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+        self.timer -= dt
+
+    def shoot(self):
+        if self.timer > 0:
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
+
+
     
