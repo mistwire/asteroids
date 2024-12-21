8f0b9e70cad61b7f51c81c0a6ff0255846a4eae3
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-12-20 19:30:29 -0500

    large and medium asteroids now split and angle off

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+import constants 
+import random
 
 
 class Asteroid(CircleShape):
@@ -11,4 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt 
+
+    def split(self):
+        self.kill()
+        if self.radius <= constants.ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+
+            angle_1 = self.velocity.rotate(random_angle)
+            angle_2 = self.velocity.rotate(-random_angle)
+
+            new_radius = self.radius - constants.ASTEROID_MIN_RADIUS
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = angle_1 * 1.2
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = angle_2 * 1.2
+
+
 
